c909de5866aaa2f8608977f97b1a5274008dabae
OhadAvitan <ohadiii100@gmail.com>
2020-12-24 11:33:20 +0200

    sprint3 ohad

--- apps/Mail/EmailApp.jsx
+++ apps/Mail/EmailApp.jsx
@@ -4,18 +4,52 @@ import { mailService } from "./services/mailService.js"
 
 export class EmailApp extends React.Component {
     state = {
-
+        filterBy: {
+            name: '',
+            isRead: false
+        },
+        emails: []
     }
 
     componentDidMount() {
-
+        console.log('Page is ready');
+        this.loadEmails()
     }
 
+
+    loadEmails = () => {
+        //before//
+        // const books = bookService.query()
+        // this.setState({ books })
+        //to async
+        mailService.query()
+            .then(emails => this.setState({ emails }))
+    };
+
+    getEmailsToDisplay = () => {
+        const { filterBy, emails } = this.state
+        console.log('emails', emails);
+
+        // return emails.filter(email => {
+        //     const isTitleInc = email.title.toLowerCase().includes(filterBy.name.toLowerCase())
+        //     const isPriceInc = email.listPrice.amount < filterBy.highPrice && book.listPrice.amount > filterBy.lowPrice
+        //     return isTitleInc && isPriceInc
+        // })
+        return emails;
+    }
+
+
     render() {
-        console.log(mailService.query());
+        
         return (
             <article>
-                <EmailList emails={mailService.query()}/>
+                <aside>
+                    <button>Inbox</button>
+                    <button>Favorites</button>
+                    <button>Sent</button>
+                    <button></button>
+                </aside>
+                <EmailList emails={this.state.emails}/>
             </article>
         )
     }
--- apps/Mail/services/mailService.js
+++ apps/Mail/services/mailService.js
@@ -1,15 +1,57 @@
+import { storageService } from "./storageService.js";
+
 export const mailService = {
    query
 }
 
+const KEY = 'emails';
 
-var emails = [
-   { id: 78, subject: 'Wassap?', body: 'Pick up!', isRead: false, sentAt: 1551133930594 },
-   { id: 79, subject: 'hi there?', body: 'Pick up!', isRead: false, sentAt: 1551133930808 },
-   { id: 76, subject: 'asd?', body: 'Pick asdasd!', isRead: false, sentAt: 1551133900000 },
-   { id: 73, subject: 'susu?', body: 'Pick up!', isRead: false, sentAt: 1551133030594 }
-]
+var emails = _createEmails()
+   // window.theBooks = emails //theEmails??????
+
+function _saveEmailsToStorage(emailsFromStorage = emails) {
+   storageService.saveToStorage(KEY, emailsFromStorage)
+}
+
+
+function _createEmails() {
+   var emailsFromStorage = storageService.loadFromStorage(KEY)
+   if (!emailsFromStorage || !emailsFromStorage.length) {
+      emailsFromStorage = [{
+            id: 78,
+            subject: 'Wassap?',
+            body: 'Pick up!',
+            isRead: false,
+            sentAt: 1551133930594
+         },
+         {
+            id: 79,
+            subject: 'hi there?',
+            body: 'Pick up!',
+            isRead: false,
+            sentAt: 1551133930808
+         },
+         {
+            id: 76,
+            subject: 'Meet in 8:30',
+            body: 'Pick lorem asdasd mi est eros convallis auctor arcu dapibus himenaeosmi est eros convallis auctor arcu dapibus himenaeos!',
+            isRead: false,
+            sentAt: 1551133900000
+         },
+         {
+            id: 73,
+            subject: 'fofo?',
+            body: 'Pick up!est eros convallis auctor arcu dapibus himenaeosmi est eros convallis auctor arcu dapibus himenaeos!est eros convallis auctor arcu dapibus himenaeosmi est eros convallis auctor arcu dapibus himenaeos!est eros convallis auctor arcu dapibus himenaeosmi est eros convallis auctor arcu dapibus himenaeos!',
+            isRead: false,
+            sentAt: 1551133030594
+         }
+      ]
+   }
+   _saveEmailsToStorage(emailsFromStorage);
+   return emailsFromStorage
+
+}
 
 function query() {
-   return emails;
+   return Promise.resolve(emails)
 }
